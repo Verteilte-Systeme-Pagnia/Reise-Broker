27a079264154d869e265bdbae6d43aca40ee94a9
Nils <nils.abels23@gmail.com>
2023-07-09 16:42:09 +0200

    DB finished

--- src/main/java/Autoverleih.java
+++ src/main/java/Autoverleih.java
@@ -5,12 +5,22 @@ import logic.ParticipantRef;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Autoverleih {
 
+    private Connection connection;
+    public Autoverleih() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/autoverleih", "root", "pass");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        try{
+        /*try{
             ArrayList<CoordinatorRef> coordinatorRefs = new ArrayList<>();
 
             coordinatorRefs.add(new CoordinatorRef(InetAddress.getByName("localhost"), Config.Coordinator1Port));
@@ -25,6 +35,89 @@ public class Autoverleih {
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
+        }*/
+
+        Autoverleih autoverleih = new Autoverleih();
+
+        String startDate = "2023-07-10";
+        String endDate = "2023-07-20";
+        try{
+            System.out.println(autoverleih.getFreeCars(startDate,endDate));
+            //System.out.println(hotel.reserveRoom(3,startDate,endDate));
+            //System.out.println(hotel.bookReservedRooms(startDate,endDate));
+            //System.out.println(hotel.checkOutdatedBookings());
+        }catch(SQLException throwables){
+            throwables.printStackTrace();
         }
     }
+
+    public int getFreeCars(String startDate, String endDate) throws SQLException {
+        this.checkOutdatedBookings();
+        String sql = "SELECT COUNT(*) FROM autos WHERE " + "reserved = 0 AND" +
+                " id NOT IN (SELECT carId FROM buchungen WHERE (startDatum >= " + "'" +  startDate + "'" + " AND startDatum <=  " + "'" + endDate + "'" + ") OR (" +
+                " endDatum >= " + "'" + startDate + "'" + " AND endDatum <= " + "'" + endDate + "'" + ") )";
+
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return rs.getInt("COUNT(*)");
+    }
+
+    public int getReservedCars() throws SQLException {
+
+        String sql = "SELECT COUNT(*) FROM autos WHERE reserved = 1";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return rs.getInt("COUNT(*)");
+    }
+
+    public  boolean reserveCar( int number, String startDate, String endDate) throws SQLException {
+        String sql = "UPDATE autos SET reserved = 1 WHERE reserved = 0 AND id NOT IN " +
+                "(SELECT carId FROM buchungen WHERE (startDatum >= " + "'" +  startDate + "'" + " AND startDatum <=  " + "'" + endDate + "'" + ") OR (" +
+                " endDatum >= " + "'" + startDate + "'" + " AND endDatum <= " + "'" + endDate + "'" + ") ) LIMIT " + number;
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        int affectedRows = ps.executeUpdate();
+        /*wenn die Anzahl der zu reservierenden Autos der Anzahl an geupdateten Zeilen entspricht, war die Datenbankoperation erfolgreich*/
+        return affectedRows == number;
+    }
+
+    public  boolean cancelReservation() throws SQLException {
+        String sql = "UPDATE autos SET reserved = 0 WHERE reserved = 1";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ps.executeUpdate();
+        /*wenn es keine reservierten Autos mehr gibt war die Datenbankoperation erfolgreich*/
+        return this.getReservedCars() == 0;
+    }
+
+    public boolean bookReservedCars(String startDate, String endDate) throws  SQLException{
+        int reservedRooms = getReservedCars();
+        int insertCount = 0;
+        for(int i = 0; i<reservedRooms;i++) {
+            String sql = "INSERT INTO buchungen (carId,startDatum,endDatum) " +
+                    "VALUES((SELECT id FROM autos WHERE reserved = 1 LIMIT 1)," + "'" + startDate + "', '" + endDate + "' )";
+            System.out.printf(sql);
+            PreparedStatement ps = this.connection.prepareStatement(sql);
+            insertCount += ps.executeUpdate();
+            sql = "UPDATE autos SET reserved = 0 WHERE reserved = 1 LIMIT 1";
+            ps = this.connection.prepareStatement(sql);
+            ps.executeUpdate();
+        }
+        /*wenn es keine reservierten Autos mehr gibt und genauso viele Buchungen,wie ursprünglich reservierte Autos erstellt wurden,
+        dann war die Datenbankoperation erfolgreich*/
+        return getReservedCars() == 0 && reservedRooms == insertCount;
+    }
+
+    public boolean checkOutdatedBookings() throws  SQLException{
+        String sql = "DELETE FROM buchungen WHERE endDatum <  CURDATE()";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ps.executeUpdate();
+        sql = "SELECT COUNT(*) FROM buchungen WHERE endDatum <  CURDATE()";
+        ps = this.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        /* wenn es keine Buchungen in der Vergangenheit gibt, war die Datenbankoperation erfolgreich*/
+        return rs.getInt("COUNT(*)") == 0;
+    }
+
 }
--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -3,11 +3,23 @@ import logic.CoordinatorRef;
 import logic.ParticipantReceive;
 import logic.ParticipantRef;
 
+import java.lang.ref.Cleaner;
 import java.net.*;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.*;
 public class Hotel {
 
+    private Connection connection;
+
+
+    public Hotel(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel", "root", "pass");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
        /* try{
             ArrayList<CoordinatorRef> coordinatorRefs = new ArrayList<>();
@@ -26,59 +38,93 @@ public class Hotel {
             throw new RuntimeException(e);
         }*/
 
+        Hotel hotel = new Hotel();
 
-        String url = "jdbc:mysql://localhost:3306/hotel";
-        String user = "root";
-        String password = "pass";
-        try {
-
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection is Successful to the database" + url);
-
-            int freeRooms = getFreeRooms(connection);
-            boolean reserve = reserveRoom(connection,1);
-            System.out.println(freeRooms);
-            System.out.println(reserve);
-            boolean cancel = cancelReservation(connection);
-            System.out.println(cancel);
-
-        } catch (SQLException throwables) {
+        String startDate = "2023-07-10";
+        String endDate = "2023-07-20";
+        try{
+            System.out.println(hotel.getFreeRooms(startDate,endDate));
+            //System.out.println(hotel.reserveRoom(3,startDate,endDate));
+            //System.out.println(hotel.bookReservedRooms(startDate,endDate));
+            //System.out.println(hotel.checkOutdatedBookings());
+        }catch(SQLException throwables){
             throwables.printStackTrace();
         }
 
 
+
+
     }
-    public static int getFreeRooms(Connection connection) throws SQLException {
-        String sql = "SELECT COUNT(*) FROM hotel WHERE status = 'frei'";
-        PreparedStatement ps = connection.prepareStatement(sql);
+    public int getFreeRooms(String startDate, String endDate) throws SQLException {
+        this.checkOutdatedBookings();
+        String sql = "SELECT COUNT(*) FROM zimmer WHERE " + "reserved = 0 AND" +
+                     " id NOT IN (SELECT roomId FROM buchungen WHERE (startDatum >= " + "'" +  startDate + "'" + " AND startDatum <=  " + "'" + endDate + "'" + ") OR (" +
+                      " endDatum >= " + "'" + startDate + "'" + " AND endDatum <= " + "'" + endDate + "'" + ") )";
+
+        PreparedStatement ps = this.connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         rs.next();
         return rs.getInt("COUNT(*)");
-
     }
 
-    public static int getReservedRooms(Connection connection) throws SQLException {
-        String sql = "SELECT COUNT(*) FROM hotel WHERE status = 'reserviert'";
-        PreparedStatement ps = connection.prepareStatement(sql);
+    public int getReservedRooms() throws SQLException {
+
+        String sql = "SELECT COUNT(*) FROM zimmer WHERE reserved = 1";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         rs.next();
         return rs.getInt("COUNT(*)");
     }
 
-    public static boolean reserveRoom(Connection connection, int number) throws SQLException {
-        String sql = "UPDATE hotel SET status = 'reserviert' WHERE status = 'frei' LIMIT " + number ;
-        PreparedStatement ps = connection.prepareStatement(sql);
+    public  boolean reserveRoom( int number, String startDate, String endDate) throws SQLException {
+        String sql = "UPDATE zimmer SET reserved = 1 WHERE reserved = 0 AND id NOT IN " +
+                     "(SELECT roomId FROM buchungen WHERE (startDatum >= " + "'" +  startDate + "'" + " AND startDatum <=  " + "'" + endDate + "'" + ") OR (" +
+                     " endDatum >= " + "'" + startDate + "'" + " AND endDatum <= " + "'" + endDate + "'" + ") ) LIMIT " + number;
+        PreparedStatement ps = this.connection.prepareStatement(sql);
         int affectedRows = ps.executeUpdate();
+        /*wenn die Anzahl der zu reservierenden Zimmer der Anzahl an geupdateten Zeilen entspricht, war die Datenbankoperation erfolgreich*/
         return affectedRows == number;
     }
 
-    public static boolean cancelReservation(Connection connection) throws SQLException {
-        String sql = "UPDATE hotel SET status = 'frei' WHERE status = 'reserviert'";
-        PreparedStatement ps = connection.prepareStatement(sql);
+    public  boolean cancelReservation() throws SQLException {
+        String sql = "UPDATE zimmer SET reserved = 0 WHERE reserved = 1";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
         ps.executeUpdate();
-
-        return getReservedRooms(connection) == 0;
+        /*wenn es keine reservierten Zimmer mehr gibt war die Datenbankoperation erfolgreich*/
+        return this.getReservedRooms() == 0;
     }
+
+    public boolean bookReservedRooms(String startDate, String endDate) throws  SQLException{
+        int reservedRooms = getReservedRooms();
+        int insertCount = 0;
+        for(int i = 0; i<reservedRooms;i++) {
+            String sql = "INSERT INTO buchungen (roomId,startDatum,endDatum) " +
+                         "VALUES((SELECT id FROM zimmer WHERE reserved = 1 LIMIT 1)," + "'" + startDate + "', '" + endDate + "' )";
+            System.out.printf(sql);
+            PreparedStatement ps = this.connection.prepareStatement(sql);
+            insertCount += ps.executeUpdate();
+            sql = "UPDATE zimmer SET reserved = 0 WHERE reserved = 1 LIMIT 1";
+            ps = this.connection.prepareStatement(sql);
+            ps.executeUpdate();
+        }
+        /*wenn es keine reservierten Zimmer mehr gibt und genauso viele Buchungen,wie ursprünglich reservierte Zimmer erstellt wurden,
+        dann war die Datenbankoperation erfolgreich*/
+        return getReservedRooms() == 0 && reservedRooms == insertCount;
+    }
+
+    public boolean checkOutdatedBookings() throws  SQLException{
+        String sql = "DELETE FROM buchungen WHERE endDatum <  CURDATE()";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ps.executeUpdate();
+        sql = "SELECT COUNT(*) FROM buchungen WHERE endDatum <  CURDATE()";
+        ps = this.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+       /* wenn es keine Buchungen in der Vergangenheit gibt, war die Datenbankoperation erfolgreich*/
+        return rs.getInt("COUNT(*)") == 0;
+    }
+
+
 
 
 }
