8fcc2cea09eb7726f86dfdd5695935ce06117f50
Adrian Hagen <adrian.hagen@sap.com>
2023-07-13 11:40:41 +0200

    UI-fertig

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,54 +4,52 @@ import java.io.IOException;
 import java.net.*;
 
 public class Client {
-    public static void main(String[] args) {
-        DatagramSocket socket = null;
+    DatagramSocket socket;
+    public Client(){
         try {
-            //Verfügbarkeit überprüfen
             socket = new DatagramSocket(Config.ClientPort);
-            //case initialize
-            byte[] msg = "checkAvailability 2002-03-03 2025-01-01".getBytes(); //Room autos fromdate todate
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String[] checkAvailability(String dateFrom, String dateTo){
+        String hotelResponse;
+        String carResponse;
+        try{
+            byte[] msg = ("checkAvailability " + dateFrom + " " + dateTo).getBytes();
             DatagramPacket sendDP = new DatagramPacket(msg,0,msg.length,InetAddress.getByName("localhost"),Config.Coordinator1Port);
             socket.send(sendDP);
 
             byte[] puffer1 = new byte[65507];
             DatagramPacket receiveDP1 = new DatagramPacket(puffer1, puffer1.length);
             socket.receive(receiveDP1);
-
-            String message = new String(receiveDP1.getData(),0,receiveDP1.getLength());
-
-            System.out.println(message);
+            hotelResponse = new String(receiveDP1.getData(),0,receiveDP1.getLength());
 
             byte[] puffer2 = new byte[65507];
             DatagramPacket receiveDP2 = new DatagramPacket(puffer2, puffer2.length);
             socket.receive(receiveDP2);
+            carResponse = new String(receiveDP2.getData(),0,receiveDP2.getLength());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new String[]{hotelResponse, carResponse};
+    }
 
-            message = new String(receiveDP2.getData(),0,receiveDP2.getLength());
-            System.out.println(message);
-
-            //das fürs buchen
-
-            /**case button buchen is pressed
-            byte[] msg = "5 4 2002-03-03 2025-01-01 Booked".getBytes(); //Room autos fromdate todate
+    public String book(String dateFrom, String dateTo, int nRooms, int nCars){
+        String response;
+        try {
+            byte[] msg = (nRooms + " " + nCars + " " + dateFrom + " " + dateTo + " Booked").getBytes();
             DatagramPacket sendDP = new DatagramPacket(msg,0,msg.length,InetAddress.getByName("localhost"),Config.Coordinator1Port);
             socket.send(sendDP);
 
             byte[] puffer = new byte[65507];
             DatagramPacket receiveDP = new DatagramPacket(puffer, puffer.length);
             socket.receive(receiveDP);
-            
-            String message = new String(receiveDP.getData(),0,receiveDP.getLength());
 
-            System.out.println(message);
-            */
-        }catch (SocketException e) {
-            throw new RuntimeException(e);
+            response = new String(receiveDP.getData(),0,receiveDP.getLength());
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-            if(socket != null){
-                socket.close();
-            }
         }
+        return response;
     }
 }
--- src/main/java/UIController.java
+++ src/main/java/UIController.java
@@ -1,101 +1,202 @@
 import javax.swing.*;
 import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 public class UIController {
+    private Client client;
+    private final Object[] shoppingCart = new Object[4];
     private JFrame frame;
-    private JTextField hotelTextField;
-    private JTextField carTextField;
-    private JButton reserveButton;
-    private JButton bookButton;
+    DateSection dateSection;
+    AmountSection amountSection;
+    ButtonSection buttonSection;
 
-    private JLabel availableHotelLabel;
-    private JLabel availableCarLabel;
+    private static class DateSection extends JPanel {
+        JTextField dateFromTextField;
+        JTextField dateToTextField;
+        public DateSection(){
+            setLayout(new GridLayout(0,4,10,10));
+            setBorder(BorderFactory.createTitledBorder(
+                    BorderFactory.createEtchedBorder(),
+                    "Bitte geben sie einen Zeitraum an."));
 
-    private int availableHotelRooms;
-    private int availableCars;
+            JLabel dateFromLabel = new JLabel("Von:");
+            add(dateFromLabel);
+
+            dateFromTextField = new JTextField();
+            dateToTextField = new JTextField();
+
+            add(dateFromTextField);
+            dateFromTextField.setColumns(10);
+
+            JLabel dateToLabel = new JLabel("Bis:");
+            add(dateToLabel);
+
+            add(dateToTextField);
+            dateToTextField.setColumns(10);
+        }
+
+        public String getDateFrom(){
+            return dateFromTextField.getText();
+        }
+
+        public String getDateTo(){
+            return dateToTextField.getText();
+        }
+    }
+
+    private static class AmountSection extends JPanel {
+        private JTextField hotelTextField;
+        private JTextField carTextField;
+        public AmountSection(){
+            setLayout(new GridLayout(0,4,10,10));
+            setBorder(BorderFactory.createTitledBorder(
+                    BorderFactory.createEtchedBorder(),
+                    "Bitte geben sie die gewünschte Anzahl an Hotelzimmern und Mietwagen an."));
+
+            JLabel hotelLabel = new JLabel("Hotelzimmer:");
+            add(hotelLabel);
+            hotelTextField = new JTextField();
+            add(hotelTextField);
+            hotelTextField.setColumns(10);
+
+            JLabel carLabel = new JLabel("Mietwagen:");
+            add(carLabel);
+            carTextField = new JTextField();
+            add(carTextField);
+            carTextField.setColumns(10);
+        }
+
+        public String getHotelAmount(){
+            return hotelTextField.getText();
+        }
+        public String getCarAmount(){
+            return carTextField.getText();
+        }
+    }
+
+    private class ButtonSection extends JPanel {
+        JButton reserveButton;
+        JButton showShoppingCartButton;
+        JButton bookButton;
+        public ButtonSection(){
+            setLayout(new GridLayout(0, 3));
+
+            reserveButton = new JButton("Verfügbarkeit prüfen");
+            reserveButton.addActionListener(e -> {
+                String dateFrom = dateSection.getDateFrom();
+                String dateTo = dateSection.getDateTo();
+                if(!validateDates(dateFrom, dateTo)){
+                    JOptionPane.showMessageDialog(frame, "Bitte geben Sie ein gültiges Datum ein.");
+                    return;
+                }
+                int nHotels;
+                int nCars;
+                try {
+                    nHotels = Integer.parseInt(amountSection.getHotelAmount());
+                    nCars = Integer.parseInt(amountSection.getCarAmount());
+                } catch (NumberFormatException nfe){
+                    JOptionPane.showMessageDialog(frame, "Bitte geben sie eine gültige Anzahl an.");
+                    return;
+                }
+                reserve(dateFrom, dateTo, nHotels, nCars);
+            });
+            add(reserveButton);
+
+            showShoppingCartButton = new JButton("Warenkorb anzeigen");
+            showShoppingCartButton.addActionListener(e -> {
+                String shoppingCartString = "Datum: " + shoppingCart[0] + " - " + shoppingCart[1] + "\n" +
+                        "Hotelzimmer: " + shoppingCart[2] + "\n" +
+                        "Mietwagen: " + shoppingCart[3];
+                JOptionPane.showMessageDialog(frame, shoppingCartString);
+            });
+            add(showShoppingCartButton);
+
+            bookButton = new JButton("Buchen");
+            bookButton.setEnabled(false); // Deaktiviert, bis Verfügbarkeit überprüft wurde
+            bookButton.addActionListener(e -> book());
+            add(bookButton);
+        }
+
+        public JButton getBookButton(){
+            return bookButton;
+        }
+    }
 
     public UIController() {
         initialize();
     }
 
     private void initialize() {
+        client = new Client();
         frame = new JFrame();
         frame.setTitle("Travel Broker");
-        frame.setBounds(100, 100, 400, 250);
+        frame.setBounds(100, 100, 500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(new GridLayout(4, 2, 10, 10));
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new GridLayout(0, 1, 30, 30));
 
-        JLabel hotelLabel = new JLabel("Hotelzimmer:");
-        frame.getContentPane().add(hotelLabel);
+        dateSection = new DateSection();
+        contentPane.add(dateSection);
 
-        hotelTextField = new JTextField();
-        frame.getContentPane().add(hotelTextField);
-        hotelTextField.setColumns(10);
+        amountSection = new AmountSection();
+        contentPane.add(amountSection);
 
-        JLabel carLabel = new JLabel("Mietwagen:");
-        frame.getContentPane().add(carLabel);
-
-        carTextField = new JTextField();
-        frame.getContentPane().add(carTextField);
-        carTextField.setColumns(10);
-
-        availableHotelLabel = new JLabel();
-        frame.getContentPane().add(availableHotelLabel);
-
-        availableCarLabel = new JLabel();
-        frame.getContentPane().add(availableCarLabel);
-
-        updateAvailability();
-
-        reserveButton = new JButton("Reservieren");
-        reserveButton.addActionListener(e -> checkAvailability(Integer.valueOf(hotelTextField.getText()), Integer.valueOf(carTextField.getText())));
-        frame.getContentPane().add(reserveButton);
-
-        bookButton = new JButton("Buchen");
-        bookButton.setEnabled(false); // Deaktiviert, bis Verfügbarkeit überprüft wurde
-        bookButton.addActionListener(e -> book(Integer.valueOf(hotelTextField.getText()), Integer.valueOf(carTextField.getText())));
-        frame.getContentPane().add(bookButton);
+        buttonSection = new ButtonSection();
+        contentPane.add(buttonSection);
     }
 
-    private void updateAvailability(){
-        // Prüfe Verfügbarkeit der Hotelzimmer und Mietwagen
-        availableHotelRooms = checkHotelAvailability();
-        availableCars = checkCarAvailability();
-
-        availableHotelLabel.setText("Verfügbare Hotelzimmer: " + availableHotelRooms);
-        availableCarLabel.setText("Verfügbare Mietwagen: " + availableCars);
+    private boolean validateDates(String dateFrom, String dateTo){
+        if(!validateDate(dateFrom) || !validateDate(dateTo)){
+            return false;
+        }
+        LocalDate from = LocalDate.parse(dateFrom);
+        LocalDate to = LocalDate.parse(dateTo);
+        if(!from.isBefore(to.plusDays(1))){
+            return false;
+        }
+        return true;
     }
 
-    private void checkAvailability(int nRooms, int nCars) {
-        if (availableHotelRooms - nRooms >= 0 && availableCars - nCars >= 0) {
-            JOptionPane.showMessageDialog(frame, "Verfügbarkeit gegeben. Sie können buchen.");
-            bookButton.setEnabled(true);
-        } else {
-            JOptionPane.showMessageDialog(frame, "Verfügbarkeit nicht gegeben. Bitte überprüfen Sie Ihre Auswahl.");
-            bookButton.setEnabled(false);
+    private boolean validateDate(String date){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
         }
     }
 
-    private int checkHotelAvailability() {
-        // Gib die Anzahl der verfügbaren Hotelzimmer zurück
-        // Implementiere hier den entsprechenden Code, um die Verfügbarkeit des Hotels zu überprüfen
-        // Hier könnte eine Anfrage an den Hotelzimmer-Anbieter gesendet werden
-        return 5; // Beispiel-Rückgabewert
+    private void reserve(String dateFrom, String dateTo, int nRooms, int nCars) {
+        String[] databaseResponse = client.checkAvailability(dateFrom, dateTo);
+        int availableHotelRooms = parseAvailabilityResponse(databaseResponse[0]);
+        int availableCars = parseAvailabilityResponse(databaseResponse[1]);
+
+        if(availableHotelRooms < nRooms || availableCars < nCars){
+            JOptionPane.showMessageDialog(frame, "Verfügbarkeit nicht gegeben. Bitte überprüfen Sie Ihre Auswahl.");
+            buttonSection.getBookButton().setEnabled(false);
+        } else {
+            shoppingCart[0] = dateFrom;
+            shoppingCart[1] = dateTo;
+            shoppingCart[2] = nRooms;
+            shoppingCart[3] = nCars;
+            JOptionPane.showMessageDialog(frame, "Verfügbarkeit gegeben. Die gewünschten Zimmer und Autos wurden dem Warenkorb hinzugefügt und können gebucht werden");
+            buttonSection.getBookButton().setEnabled(true);
+        }
     }
 
-    private int checkCarAvailability() {
-        // Gib die Anzahl der verfügbaren Mietwagen zurück
-        // Implementiere hier den entsprechenden Code, um die Verfügbarkeit des Mietwagens zu überprüfen
-        // Hier könnte eine Anfrage an den Mietwagen-Anbieter gesendet werden
-        return 3; // Beispiel-Rückgabewert
+    private int parseAvailabilityResponse(String response){
+        String[] responseSplit = response.split(" ");
+        return Integer.parseInt(responseSplit[responseSplit.length - 1]);
     }
 
-    private void book(int nRooms, int nCars) {
-        // Führe die verbindliche Buchung für Hotelzimmer und Mietwagen durch
-        // Implementiere hier den entsprechenden Code, um die Buchung durchzuführen
-        // Hier könnten Anfragen an die entsprechenden Anbieter gesendet werden
+    private void book() {
+        client.book((String)shoppingCart[0], (String)shoppingCart[1], (int)shoppingCart[2], (int)shoppingCart[3]);
 
-        JOptionPane.showMessageDialog(frame, "Buchung abgeschlossen. Vielen Dank!");
+        JOptionPane.showMessageDialog(frame, "Herzlichen Dank für ihre Buchung bei den Pagnia Piranhas.");
     }
 
     public void show() {
